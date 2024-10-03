5e451296065ae66f3996b0b00dd16cda6a9abc52
Lev <misiliuk.lev@gmail.com>
2024-10-03 13:54:31 +0300

    for deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,  // Вимикає оптимізацію зображень
+  },
   exportPathMap: function () {
     return {
       '/': { page: '/' },
